a04091fc579fb91dfed35a08ef02af966ef55e0b
Mario Juarez <mario@mjp.one>
2017-02-22 11:47:54 +0100

    Initial commit

---
+++ tests/Example/BTest.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace Tests\Example;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class BTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    public function testA()
+    {
+        // nothing
+    }
+
+    public function testB()
+    {
+        // nothing
+    }
+
+    public function testC()
+    {
+        // nothing
+    }
+
+    public function testD()
+    {
+        // nothing
+    }
+
+    public function testE()
+    {
+        // nothing
+    }
+
+    public function testF()
+    {
+        // nothing
+    }
+
+    public function testG()
+    {
+        // nothing
+    }
+
+    public function testH()
+    {
+        // nothing
+    }
+
+    public function testI()
+    {
+        // nothing
+    }
+
+    public function testJ()
+    {
+        // nothing
+    }
+
+    public function testK()
+    {
+        // nothing
+    }
+
+    public function testL()
+    {
+        // nothing
+    }
+
+    public function testM()
+    {
+        // nothing
+    }
+
+    public function testN()
+    {
+        // nothing
+    }
+
+    public function testO()
+    {
+        // nothing
+    }
+
+    public function testP()
+    {
+        // nothing
+    }
+
+    public function testQ()
+    {
+        // nothing
+    }
+
+    public function testR()
+    {
+        // nothing
+    }
+
+    public function testS()
+    {
+        // nothing
+    }
+
+    public function testT()
+    {
+        // nothing
+    }
+
+    public function testU()
+    {
+        // nothing
+    }
+
+    public function testV()
+    {
+        // nothing
+    }
+
+    public function testW()
+    {
+        // nothing
+    }
+
+    public function testX()
+    {
+        // nothing
+    }
+
+    public function testY()
+    {
+        // nothing
+    }
+
+    public function testZ()
+    {
+        // nothing
+    }
+}
